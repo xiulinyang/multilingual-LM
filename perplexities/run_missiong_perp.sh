11c9a6f14626b33dbc22df42ef1c1cddc2545d76
xiulinyang <xiulin.yang.compling@gmail.com>
2025-01-08 18:45:00 +0100

    udpate results

--- perplexities/run_missiong_perp.sh
+++ perplexities/run_missiong_perp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python perplexities_exp.py shuffle_even_odd shuffle_even_odd ZH 81 randinit pretrained
+CUDA_VISIBLE_DEVICES=0 python perplexities_exp.py shuffle_even_odd shuffle_even_odd ZH 41 randinit pretrained
