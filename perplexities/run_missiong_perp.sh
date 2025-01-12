7ab9ba9e325c37f7952c243d9791a09a0017b7d0
xiulinyang <xiulin.yang.compling@gmail.com>
2025-01-12 14:30:17 +0100

    update

--- perplexities/run_missiong_perp.sh
+++ perplexities/run_missiong_perp.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python perplexities_exp.py shuffle_local_word3 shuffle_local_word3 IT 53 randinit pretrained
+CUDA_VISIBLE_DEVICES=3 python perplexities_exp.py shuffle_remove_fw shuffle_remove_fw ZH 81 randinit pretrained
+
+CUDA_VISIBLE_DEVICES=3 python perplexities_exp.py shuffle_remove_fw shuffle_remove_fw ZH 41 randinit pretrained
