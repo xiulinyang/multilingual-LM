27d643b025568cb11c110ec0a99e29d7caa3299d
xiulinyang <xiulin.yang.compling@gmail.com>
2025-01-13 21:33:45 +0100

    update results

--- perplexities/run_missiong_perp.sh
+++ perplexities/run_missiong_perp.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 
-CUDA_VISIBLE_DEVICES=0 python perplexities_exp.py perturb_num_adj perturb_num_adj EN 53 randinit pretrained
-CUDA_VISIBLE_DEVICES=0 python perplexities_exp.py perturb_num_adj perturb_num_adj EN 81 randinit pretrained
+CUDA_VISIBLE_DEVICES=0 python perplexities_exp.py shuffle_local3 shuffle_local3 PL 41 randinit pretrained
+CUDA_VISIBLE_DEVICES=0 python perplexities_exp.py shuffle_local5 shuffle_local5 PL 53 randinit pretrained
+CUDA_VISIBLE_DEVICES=0 python perplexities_exp.py shuffle_local10 shuffle_local10 PL 81 randinit pretrained
+CUDA_VISIBLE_DEVICES=0 python perplexities_exp.py shuffle_even_odd shuffle_even_odd PL 41 randinit pretrained
+CUDA_VISIBLE_DEVICES=0 python perplexities_exp.py shuffle_even_odd shuffle_even_odd PL 53 randinit pretrained
