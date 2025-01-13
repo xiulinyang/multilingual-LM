d4f2cd557d0d50ff07abca8ad2321cb13f914d54
Xiulin Yang <xiulinyang@Xiulins-MacBook-Pro-4.local>
2025-01-13 00:28:43 -0500

    update code

--- training/multilingual_dataset.py
+++ training/multilingual_dataset.py
@@ -30,7 +30,8 @@ _FUNCTIONS = [
     "shuffle_deterministic_word21",
     "shuffle_remove_fw",
     "perturb_num_adj",
-    "perturb_adj_num"
+    "perturb_adj_num",
+    "shuffle_control"
 ]
 _TRAIN_SETS = ["DE","TR","RU","RO","AR","NL","ZH","PL","PT","FR","IT","EN"]
 _PERTURBATIONS = [f"{p}_{lang.lower()}" for p in _FUNCTIONS for lang in _TRAIN_SETS]
