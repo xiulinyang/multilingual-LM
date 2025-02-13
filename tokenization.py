f9eb88d0a16434f34f61aca31a4e48492f0a457e
Xiulin Yang <xiulinyang@Xiulins-MacBook-Pro-5.local>
2025-02-12 22:41:25 -0500

    update code

---
+++ tokenization.py
@@ -0,0 +1,27 @@
+from transformers import AutoTokenizer, AddedToken,GPT2LMHeadModel, GPT2Tokenizer,AutoTokenizer, AutoModel
+from pathlib import Path
+
+TOKENIZER_DICT = {
+   "EN": "gpt2",
+   "DE": "dbmdz/german-gpt2",
+   "RU": "sberbank-ai/rugpt3large_based_on_gpt2",
+   "RO": "dumitrescustefan/gpt-neo-romanian-780m",
+   "TR": "ytu-ce-cosmos/turkish-gpt2",
+   "FR": "lightonai/pagnol-xl",
+   "NL": "yhavinga/gpt-neo-125M-dutch",
+   "IT": "iGeniusAI/Italia-9B-Instruct-v0.1",
+   "PL":"flax-community/papuGaPT2",
+    "PT": "NOVA-vision-language/GlorIA-1.3B'",
+    "ZH": "hfl/chinese-bert-wwm",
+    "AR": "aubmindlab/aragpt2-base"}
+
+for lang in TOKENIZER_DICT:
+    tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_DICT[lang])
+    sents = Path(f'data/multilingual/{lang}/train/{lang}.train','r').read_text().strip().split('\n')
+    token_num =0
+    for sent in sents:
+        tokens = tokenizer.tokenize(sent)
+        token_num += len(tokens)
+
+    average = token_num/len(sents)
+    print(lang, average)
